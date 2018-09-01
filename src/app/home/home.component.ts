96dff9084ba0f2ae2f1199a996215744f1f2dd80
k4the <k4thesmith@gmail.com>
2018-09-01 15:23:23 +0100

    all files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { spin } from '../spin.animation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [spin]
 })
 export class HomeComponent implements OnInit {
+  spinState = 'forward';
+  constructor( private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  gotoPage(): void {
+    this.spin();
+    this.router.navigate(['/what']);
   }
 
+  spin(): void {
+    this.spinState === 'forward' ? this.spinState = 'backward' : this.spinState = 'forward';
+  }
 }
