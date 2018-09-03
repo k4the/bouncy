f487962a31410af9a71b4953dfc9c61a2e7cfafd
k4the <k4thesmith@gmail.com>
2018-09-03 06:36:45 +0100

    refs

--- src/app/refs-and-inspiration/refs-and-inspiration.component.ts
+++ src/app/refs-and-inspiration/refs-and-inspiration.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RefsAndInspirationComponent implements OnInit {
 
+  links: any[] = [
+    {
+      title: 'Advantages Of Decoupling Application Front-End From Back-End',
+      author: 'Abhinav Asthana, CEO & Co-founder of Postman',
+      url: 'https://www.expresscomputer.in/news/advantages-of-decoupling-application-front-end-from-back-end/24245/'
+    },
+    {
+      title: 'Angular Single Page Applications (SPA): What are the Benefits?',
+      author: 'Angular University',
+      url: 'https://blog.angular-university.io/why-a-single-page-application-what-are-the-benefits-what-is-a-spa/'
+    },
+    {
+      title: 'Is My Single-Page Application SEO Friendly?',
+      author: 'Anthony Gore, Vue Developers',
+      url: 'https://vuejsdevelopers.com/2018/04/09/single-page-app-seo/'
+    },
+    {
+      title: 'Use Fetch as Google for websites',
+      author: 'Google',
+      url: 'https://support.google.com/webmasters/answer/6066468?hl=en?utm_campaign=chrome_series_fetchgoogleweb_111717'
+    },
+    {
+      title: 'HTML5',
+      author: 'Wikipedia',
+      url: 'https://en.wikipedia.org/wiki/HTML5'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
